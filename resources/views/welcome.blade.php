47d00bd3b7c3691666425b2f3fe2382ea7d1f8ba
Emanuel V <emanuel.205m@gmail.com>
2020-12-02 00:44:29 -0700

    Todo por mientras

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Sistema de Eventos Formativos</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link href="{{ URL::asset('/css/style.css') }}" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="cuadrologin">
+            <a href="registro">Login</a>
+        </div>
+        <div class="topnav">
+             <img src="{{ URL::asset('img/logo-institucional.jpg') }}" height="150" width="150" alt="logo">
+             <div class="anclas">
+                 <a class="active" href="home">Principal</a>
+                 <a href="eventos">Eventos Formativos</a>
+                 <a href="eventospersonales">Tus Eventos</a>
+                 <a href="informacion">Información</a>
+             </div>
+        </div>
+<!--
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Bienvenido
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://vapor.laravel.com">Vapor</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
+    -->
+    </body>
+</html>
