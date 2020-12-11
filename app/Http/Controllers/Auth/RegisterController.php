16088ef7c9e5633555a46bc624f8faee6bafa1b6
jesuscarmona12 <33427383+jesuscarmona12@users.noreply.github.com>
2020-12-11 02:54:41 -0700

    registro agregadi

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
-use App\User;
+use App\Usuario;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-
+use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     /*
@@ -47,6 +47,24 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
+    public function index(){
+        return view('auth.register');
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return Usuario::create([
+            'nombreUsuario' => $data['name'],
+            'correo' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
     protected function validator(array $data)
     {
         return Validator::make($data, [
@@ -62,12 +80,30 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['nombre'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+    public function store(Request $request){
+        $user = new Usuario();
+        //dd($request);
+        $request->validate([
+            'nombre' => 'required|min:4|max:30',
+            'email'=> 'required|email|unique:usuario,correo',
+            'password' => 'required|min:3|confirmed'
         ]);
+        
+        //dd($request);
+
+            
+            $user->nombreUsuario = $request ->input('nombre');
+            $user->apellidoUsuario =$request ->input('apellido');
+            $user->correo = $request-> input('email');
+            $user->password = bcrypt(request('password'));
+            $user->esAdmin = 0;
+            $user->esInstancia = 0;
+
+            $user->save();
+            
+            return redirect('login');
+       
+       
+        //return back()->withErrors(['email' => 'Los datos no son validos'])->withInput(request(['email']));
     }
 }
